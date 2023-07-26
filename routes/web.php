6d22b2d28b40ab446e1701fc074666390e028a80
Paweł Blonkowski <ravandil123@gmail.com>
2023-07-26 12:49:56 +0200

    Added user list with routing and controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,8 @@ use App\Http\Controllers\HomeController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users/List', [UserController::class, 'index'])->middleware('auth');
 
 Auth::routes();
 
